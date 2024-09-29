9334950ba21f589b200633dcd6abf4b2b18f404c
AlexStue <alexstue@gmx.de>
2024-09-29 12:29:26 +0200

    ingress added

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,6 @@
 provider "local" {}
-
-# ...
+#-----------------------------------------------------------------
+# Install k3s
 resource "null_resource" "k3s_setup" {
   provisioner "local-exec" {
     command = <<-EOT
@@ -15,23 +15,25 @@ resource "null_resource" "k3s_setup" {
       sudo chmod 644 /etc/rancher/k3s/k3s.yaml
     EOT
   }
-
   triggers = {
     always_run = "${timestamp()}"
   }
 }
 
-# ...
+#-----------------------------------------------------------------
+# Deploy k3s namespace DEV
 resource "null_resource" "apply_k3s_deployment" {
   provisioner "local-exec" {
     command = <<-EOT
       echo "Creating Kubernetes dev"
       kubectl create namespace dev
       echo "Applying Kubernetes deployment"
-      kubectl apply -f /home/ubuntu/petclinic-jul24/infra/k3s/petclinic-combined.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24/infra/k3s/nginx-combined.yml
+      #kubectl apply -f /home/ubuntu/petclinic-jul24/infra/k3s/1-basic-way/petclinic-combined.yml
+      #kubectl apply -f /home/ubuntu/petclinic-jul24/infra/k3s/1-basic-way/nginx-combined.yml
+      
+      kubectl apply -f /home/ubuntu/petclinic-jul24/infra/k3s/2-with-ingress/nginx-comb-v2.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24/infra/k3s/2-with-ingress/ingress-comb-v2.yml
     EOT
   }
-
   depends_on = [null_resource.k3s_setup]
 }

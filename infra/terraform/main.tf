33c1e724062ff6e0723a809cd4e15221feb26e8f
Rustingheart <hierzi.p@gmail.com>
2024-10-20 09:35:43 +0200

    database+ingress vp

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,9 +10,11 @@ resource "null_resource" "apply_k3s_manifests" {
       kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/monitoring/pmth-svr-ConfigMap.yml
 
       ### Deployments ###
-
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/app-combined.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/ingress-resource.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/secrets/tls-secret.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/secrets/db-secret.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/vp/app-combined.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/vp/ingress-combined.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/vp/db-combined.yml
       kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/mon-pmth-ClusterRole.yml
       kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/mon-pmth-exp-NodeExporter.yml
       kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/8-App-mon-ingr/mon-pmth-svr-comb.yml

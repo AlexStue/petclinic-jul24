7d583b0ea93875b8d922f0e392caa4e9246ad0e3
AlexStue <alexstue@gmx.de>
2024-10-21 23:29:44 +0200

    draft. Terraform Cloud

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,5 @@
 terraform { 
   cloud { 
-    
     organization = "alexstue-petclinic-jul24" 
 
     workspaces { 
@@ -9,38 +8,12 @@ terraform {
   } 
 }
 
-provider "local" {}
-resource "null_resource" "apply_k3s_manifests" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      echo "Applying Kubernetes deployments"
-      
-      ### Configs ###
+# Step 1: Configure the Kubernetes provider
+provider "kubernetes" {
+  config_path = "~/.kube/config" # Use the path to your kubeconfig file
+}
 
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/monitoring/grfa-ConfigMap.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/monitoring/pmth-svr-ConfigMap.yml
-
-      ### Deployments ###
-
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/app-combined.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/ingress-resource.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-ClusterRole.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-exp-NodeExporter.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-svr-comb.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-almg-comb.yml
-      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-grfa-comb.yml
-
-      ### restarts ###
-
-      #kubectl rollout restart deployment mysqlserver-deployment -n dev
-      #kubectl rollout restart deployment petclinic-deployment -n dev
-      #kubectl rollout restart deployment prometheus-deployment -n dev
-      #kubectl rollout restart deployment alertmanager-deployment -n dev
-      #kubectl rollout restart deployment grafana-deployment -n dev
-
-    EOT
-  }
-  triggers = {
-    always_run = "${timestamp()}"
-  }
+# Step 4: Use the kubernetes_manifest resource to apply the Kubernetes manifest
+resource "kubernetes_manifest" "mon_grfa_comb" {
+  manifest = yamldecode(file("/home/ubuntu/petclinic-jul24-deploy/k3s/mon-grfa-comb.yml"))
 }

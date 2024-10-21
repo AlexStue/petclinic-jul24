417941f66e9cd707da7a58330d5fb5981369a4ab
AlexStue <alexstue@gmx.de>
2024-10-21 23:09:52 +0200

    draft. Terraform Cloud

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,10 +1,46 @@
-terraform { 
-  cloud { 
+# terraform { 
+#   cloud { 
     
-    organization = "alexstue-petclinic-jul24" 
+#     organization = "alexstue-petclinic-jul24" 
 
-    workspaces { 
-      name = "petclinic-jul24-cli" 
-    } 
-  } 
+#     workspaces { 
+#       name = "petclinic-jul24-cli" 
+#     } 
+#   } 
+# }
+
+provider "local" {}
+resource "null_resource" "apply_k3s_manifests" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo "Applying Kubernetes deployments"
+      
+      ### Configs ###
+
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/monitoring/grfa-ConfigMap.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/monitoring/pmth-svr-ConfigMap.yml
+
+      ### Deployments ###
+
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/app-combined.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/ingress-resource.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-ClusterRole.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-exp-NodeExporter.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-svr-comb.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-pmth-almg-comb.yml
+      kubectl apply -f /home/ubuntu/petclinic-jul24-deploy/k3s/mon-grfa-comb.yml
+
+      ### restarts ###
+
+      #kubectl rollout restart deployment mysqlserver-deployment -n dev
+      #kubectl rollout restart deployment petclinic-deployment -n dev
+      #kubectl rollout restart deployment prometheus-deployment -n dev
+      #kubectl rollout restart deployment alertmanager-deployment -n dev
+      #kubectl rollout restart deployment grafana-deployment -n dev
+
+    EOT
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 }
